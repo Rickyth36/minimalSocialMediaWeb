b9f149c680b9a5bd2b437150771f5c4f19752ab1
Ricky Thoudam <110334937+Rickyth36@users.noreply.github.com>
2025-02-14 00:17:16 +0530

    post functionalities added

--- app.js
+++ app.js
@@ -20,12 +20,26 @@ app.get("/", (req, res) => {
     res.render('index');
 })
 
-app.get("/profile", isLoggedIn, (req, res) => {
-    res.send('Profile page');
+app.get("/profile", isLoggedIn, async(req, res) => {
+    let user = await userModel.findOne({ email: req.user.email }).populate("posts");
+    console.log(user);
+    res.render('profile',{user});
 })
 
 app.get("/login", (req, res) => {
     res.render('login');
+})
+
+app.post("/post", isLoggedIn, async(req, res) => {
+    let user = await userModel.findOne({ email: req.user.email });
+    let { content } = req.body;
+    let post = await postModel.create({
+        user: user._id,
+        content
+    })
+    user.posts.push(post._id);
+    await user.save();
+    res.redirect("/profile");
 })
 
 app.post("/register", async(req, res) => {
@@ -59,7 +73,7 @@ app.post("/login", async(req, res) => {
         if (result) {
             let token = jwt.sign({ email: email, userid: user._id }, 'secret');
             res.cookie('token', token);            
-            res.status(200).send('Login successful');
+            res.status(200).redirect('/profile');
         }   
         else res.redirect("/login"); 
     })
@@ -74,10 +88,11 @@ app.get("/logout", (req, res) => {
 
 function isLoggedIn(req,res,next){
     if (req.cookies.token === "") {
-        res.send('You must be logged in first')
+        res.redirect("/login")
     }
     else {
         let data = jwt.verify(req.cookies.token, "secret");
+        req.user = data;
         next();
     }
 }
